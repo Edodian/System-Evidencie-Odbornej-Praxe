93b70b8d9cff940737c1d5a7a533aa9767e27449
klementinko <andrei.parfirev@student.ukf.sk>
2025-11-05 21:47:09 +0100

    Temporary turned off security and CORS for testing purpoces, will set it up later

--- Backend/src/main/java/sk/ukf/sep/config/CorsConfig.java
+++ Backend/src/main/java/sk/ukf/sep/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         var config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        // config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
--- Backend/src/main/java/sk/ukf/sep/config/SecurityConfig.java
+++ Backend/src/main/java/sk/ukf/sep/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package sk.ukf.sep.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,25 +18,29 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // отключаем CSRF для REST API
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/student").permitAll() // логин и регистрация разрешены без авторизации
-                        .anyRequest().authenticated() // остальное требует логина
-                )
-                .formLogin(Customizer.withDefaults()) // стандартная обработка логина
-                .logout(Customizer.withDefaults()) // стандартный logout
-                .cors(Customizer.withDefaults()); // включаем CORS
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(Customizer.withDefaults());
 
         return http.build();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable()) // отключаем CSRF для REST API
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/login", "/student").permitAll() // логин и регистрация разрешены без авторизации
+//                        .anyRequest().authenticated() // остальное требует логина
+//                )
+//                .formLogin(Customizer.withDefaults()) // стандартная обработка логина
+//                .logout(Customizer.withDefaults()) // стандартный logout
+//                .cors(Customizer.withDefaults()); // включаем CORS
+//
+//        return http.build();
+//    }
 }
